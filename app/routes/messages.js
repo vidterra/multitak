f1dedf2bdcd31b82add3101af085c01f79d9ee4b
Dillon Bussert <dillon.bussert@vidterra.com>
2021-09-07 09:01:26 -0400

    Add API for COT messages

--- app/routes/messages.js
+++ app/routes/messages.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const router = express.Router()
+const xmljs = require('xml-js')
+const {cot} = require("@vidterra/tak.js");
 
-router.get('/api/messages', async (req, res) => {
+router.get('/api/messages', (req, res) => {
 	return res.send(cotHistory.map(cot => {
 		delete cot.raw
 		return cot
 	}))
 })
 
-router.delete('/api/messages', async (req, res) => {
+router.delete('/api/messages', (req, res) => {
 	global.cotHistory.length = 0
-	return res.send({ status: 'OK' })
+	return res.send({status: 'OK'})
+})
+
+router.post('/api/messages', (req, res) => {
+	const message = req.body.message
+
+	if(!message) {
+		return res.status(400).send({
+			status: 'FAIL',
+			message: `Message missing`
+		})
+	}
+
+	const packet = {
+		date: Date.now(),
+		source: {
+			type: 'api',
+			ip:  req.headers['x-forwarded-for'] || req.socket.remoteAddress
+		},
+		raw: cot.js2xml(message),
+		message
+	}
+
+	messageEmitter.emit('cotAdd', packet)
+	return res.send('OK')
 })
 
 module.exports = router
